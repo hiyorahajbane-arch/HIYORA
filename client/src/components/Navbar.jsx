c37bcf223339e5256bc0fb57cca1396d61c28cf1
souk <souk@local.test>
2026-09-17 22:44:30 +0100

    add HIYORA logo to navbar footer favicon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,8 @@ export default function Navbar() {
       <header className="navbar">
         <div className="navbar-inner">
           <Link to="/" className="brand">
-            {t('brand')}<small>{t('brandSub')}</small>
+            <img src="/images/logo-hiyora.jpeg" alt="HIYORA" className="brand-logo" />
+            <span className="brand-text">{t('brand')}<small>{t('brandSub')}</small></span>
           </Link>
           <div className="nav-links">
             <NavLink to="/" end>{t('home')}</NavLink>
